fc812463430158a35fa69be7670849c14558cfd4
Joao Gosmani <joaogosmani@gmail.com>
2022-12-13 15:54:44 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TESTE 1: seller findById ===");
-		Seller seller = sellerDao.findById(1);
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 		System.out.println("\n=== TESTE 2: seller findByDepartment ===");
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TESTE 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 		
 	}
 
